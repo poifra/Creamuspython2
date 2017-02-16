230f2af4e6d05e3d968d909a1b75ef4ad0af81f9
Francois <fpoitras123@gmail.com>
2017-02-16 08:53:54 -0500

    changed test to make it more interesting

--- test.py
+++ test.py
@@ -6,15 +6,22 @@ from random import choice
 from pyo import *
 
 s = Server().boot()
+
+#assuming 4/4 time
 durees = {
 	"whole":1,
-	"half":0.5,
-	"quarter":0.25,
-	"eigth":0.125,
-	"sixteenth":0.0625
+	"half":1/2,
+	"quarter":1/4,
+	"eigth":1/8,
+	"triple":1/12,
+	"sixteenth":1/16,
 }
-realNotes = [Note(i, durees["sixteenth"]) for i in transpose(target='minor', octave=6, key='G')]
-seq = Sequence(realNotes,tempo=60)
+
+scale =  transpose(target='minor', octave=6, key='G') + transpose(target='minor', octave=6, key='G')[::-1]
+
+realNotes = [Note(i, durees["eigth"]) for i in scale]
+
+seq = Sequence(realNotes,tempo=80)
 syn = ClassicSynth(seq)
 
 syn.sequence.play()
