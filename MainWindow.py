1aa51bcfea17bac9200f6398964677a8b7cfd282
Francois <fpoitras123@gmail.com>
2017-03-22 21:47:09 -0400

    added inversion box

--- MainWindow.py
+++ MainWindow.py
@@ -4,10 +4,11 @@ from Chordbook import chords, chordSymbols, shiftFactors
 
 class CustomFrame(wx.Frame):
 	def __init__(self, parent, id, title):
-		wx.Frame.__init__(self, parent, id, title, (-1, -1), wx.Size(450, 330), style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
+		wx.Frame.__init__(self, parent, id, title, (-1, -1), wx.Size(450, 375), 
+			style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
 
-		notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
-		
+		notes = sorted(['C','C#','D','D#','Db','E','Eb','F','F#','G','G#','Gb','A','A#','Ab','B','Bb'])
+
 		self.chords = [u'Dm7',u'G7',u'C7'] #initial progression
 		self.panel = wx.Panel(self, -1)
 
@@ -30,8 +31,10 @@ class CustomFrame(wx.Frame):
 			choices = sorted(shiftFactors.keys()), style = wx.LB_SINGLE | wx.LB_ALWAYS_SB)
 		self.chordQualities = wx.ListBox(self.panel, -1, pos=wx.Point(32, 64),size=wx.Size(184, 256),
 			choices = sorted(chords.keys()), style = wx.LB_SINGLE | wx.LB_ALWAYS_SB)
-		self.inversion = wx.ComboBox(self.panel, size=wx.DefaultSize, choices=notes)
 		
+		self.inversion = wx.ComboBox(self.panel, size=wx.DefaultSize, choices=notes)
+		self.inversionLabel = wx.StaticText(self.panel, id=-1, label="Inversion : ")
+
 		self.containerSizer = wx.BoxSizer(wx.VERTICAL)
 		self.chordChooserSizer = wx.BoxSizer(wx.HORIZONTAL)
 		self.inversionSizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -45,13 +48,18 @@ class CustomFrame(wx.Frame):
 		self.btnPlayStopSizer.Add(self.btnPlay, 1, wx.EXPAND)
 		self.btnPlayStopSizer.Add(self.btnStop, 1, wx.EXPAND)
 
-		self.chordChooserSizer.Add(self.chordKeys, 1, wx.EXPAND)
-		self.chordChooserSizer.Add(self.chordQualities, 1, wx.EXPAND)
+		self.chordChooserSizer.Add(self.chordKeys, 1, wx.LEFT, 100)
+		self.chordChooserSizer.Add(self.chordQualities, 1, wx.RIGHT, 100)
+
+		self.inversionSizer.Add(self.inversionLabel, 0, wx.LEFT | wx.ALIGN_CENTER,150)
+		self.inversionSizer.Add(self.inversion, 1, wx.RIGHT,150)
 
 		self.displaySizer.Add(self.chordLabel, 1 , wx.EXPAND)
 
 		self.containerSizer.Add(self.chordChooserSizer, 0, wx.EXPAND)
+		self.containerSizer.Add(self.inversionSizer, 0, wx.EXPAND)
 		self.containerSizer.Add(self.btnAddRemoveSizer, 0, wx.EXPAND)
+		self.containerSizer.Add(wx.StaticLine(self.panel), 0, wx.ALL|wx.EXPAND, 5)
 		self.containerSizer.Add(self.displaySizer, 0, wx.ALIGN_CENTER)
 		self.containerSizer.Add(self.btnPlayStopSizer, 0, wx.EXPAND)
 		
@@ -63,16 +71,31 @@ class CustomFrame(wx.Frame):
 		self.displaySizer.Layout()
 		self.containerSizer.Layout()
 
+	def _updateBoxes(self):
+		self.inversion.SetSelection(-1)
+		self.chordQualities.SetSelection(-1)	
+		self.chordKeys.SetSelection(-1)
+		self.inversionSizer.Layout()
+
 	def onAddChord(self, event):
 		#stack based logic, will change in the future
+		inv = ''
 		if self.chordQualities.GetSelection() == -1 or self.chordKeys.GetSelection() == -1:
 			print "YOU MUST CHOOSE ADDITIONAL DATA"
 			return
+		if self.inversion.GetSelection != -1:
+			inv = self.inversion.GetString(self.inversion.GetSelection())
 
 		key = self.chordKeys.GetString(self.chordKeys.GetSelection())
 		quality = self.chordQualities.GetString(self.chordQualities.GetSelection())
 		symbol = chordSymbols[quality]
-		self.chords.append(key+symbol)
+
+		if inv == '':
+			self.chords.append(key+symbol)
+		else:
+			self.chords.append(key+symbol+'/'+inv)
+
+		self._updateBoxes()
 		self._updateLabel()
 
 	def onDeleteChord(self, event):
